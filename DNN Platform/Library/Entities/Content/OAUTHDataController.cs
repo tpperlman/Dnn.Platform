c390fa089717d7505836a951d7b614d4167fa471
Cathal Connolly <cathal@dnncorp.com>
2015-07-09 18:00:57 +0100

    Initial commit

---
+++ DNN Platform/Library/Entities/Content/OAUTHDataController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetNuke.Data;
+
+namespace DotNetNuke.Entities.Content
+{
+    public class OauthDataController
+    {
+        public void ClientRepositoryGetById()
+        {
+          //  DataProvider.Instance().ExecuteNonQuery("DeleteJavaScriptLibrary", library.JavaScriptLibraryID);
+            //ClearCache();
+        }
+    }
+}
